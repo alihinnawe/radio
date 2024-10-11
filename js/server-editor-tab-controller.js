035ff7d997e47eb31ea769e79a7a25b6b7a9fbab
hinnaweali <hinnaweali@gmail.com>
2024-10-11 12:35:40 +0200

    add the album template and the tracks template

--- js/server-editor-tab-controller.js
+++ js/server-editor-tab-controller.js
@@ -5,7 +5,7 @@ import TabController from "../../../tool/tab-controller.js";
  * Skeleton for tab controller type.
  */
 class EditorTabController extends TabController {
-
+	#album;
 	/**
 	 * Initializes a new instance.
 	 */
@@ -22,6 +22,9 @@ class EditorTabController extends TabController {
 	get viewsSectionSection () { return document.querySelector("section.server-editor"); }
 
 	get editorSectionTemplate () { return document.querySelector("head>template.server-album-editor"); }
+	get serverAlbumEditorSection () { return this.center.querySelector("section.server-album-editor"); }
+	get editorTrackTemplate () { return document.querySelector("head>template.server-album-editor-row"); }
+	get serverAlbumEditorSectionTable () { return this.center.querySelector("section.server-album-editor>div.tracks>div.data>table>tbody"); }
 
 
 	/**
@@ -32,7 +35,7 @@ class EditorTabController extends TabController {
 		while (this.center.lastElementChild) this.center.lastElementChild.remove();
 		this.center.append(section);
 
-		this.viewsSectionSection.querySelector("div.control>button.create").addEventListener("click",event => this.proccessDisplayAlbumEditor());
+		this.viewsSectionSection.querySelector("div.control>button.create").addEventListener("click",event => this.processDisplayAlbumEditor());
 
 	     //const albums = this.#invokeQueryAllAlbums();
 		//console.log("albums",albums)
@@ -43,27 +46,43 @@ class EditorTabController extends TabController {
 	}
 
 
-	async #invokeQueryAllAlbums () {
-		const resource = this.sharedProperties["service-origin"] + "/services/albums";
-		console.log("resource",resource);
-		const headers = { "Accept": "application/json" };
-		const response = await fetch(resource, { method: "GET" , headers: headers, credentials: "include" });
-		if (!response.ok) throw new Error("HTTP " + response.status + " " + response.statusText);
-		return response.json();
-	}
 
-
-	async proccessDisplayAlbumEditor () { 
+	async processDisplayAlbumEditor() {
 		const sessionOwner = this.sharedProperties["session-owner"];
 		this.viewsSectionSection.classList.add("hidden");
+	
+		// Clone editor section template
 		const tableRow = this.editorSectionTemplate.content.firstElementChild.cloneNode(true);
-		const imageCover = this.sharedProperties["service-origin"] + "/services/documents/" + sessionOwner.avatar.identity;
-		console.log("imageCover",imageCover);
 		this.center.append(tableRow);
+	
+		// Set cover image source
+		const imageCoverSource = this.sharedProperties["service-origin"] + "/services/documents/" + sessionOwner.avatar.identity;
+		const imageCoverAvatar = tableRow.querySelector("div.album>span.cover>button>img");
+		imageCoverAvatar.src = imageCoverSource;
+	
+		// Add event listener for the save button
+		const speichernButton = this.serverAlbumEditorSection.querySelector("div.control>button.submit");
+		speichernButton.addEventListener("click", event => {
+			// Get the updated values of the inputs when the save button is clicked
+			const title = this.serverAlbumEditorSection.querySelector("div.album>span.other>div.title>input").value.trim();
+			const releaseYear = this.serverAlbumEditorSection.querySelector("div.album>span.other>div.release-year>input").value.trim();
+			const trackCount = this.serverAlbumEditorSection.querySelector("div.album>span.other>div.track-count>input").value.trim();
+			
+			// Update the album object with the latest input values
+			this.#album = { title, releaseYear, trackCount };
+			
+			// Call the function to create or update the album
+			this.#invokeCreateOrUpdateAlbum(this.#album);
+			const savedAlbums = this.#invokeQueryAllAlbums();
+			console.log("list of all albums",savedAlbums);
+			console.log("album identity",this.#invokeCreateOrUpdateAlbum(this.#album));
+			
+		});
 
-		const createdElement = await this.#invokeCreateOrUpdateAlbum(album);
-		console.log("createdElementcreatedElement",createdElement);
+		const buttonTrack = this.serverAlbumEditorSection.querySelector("div.tracks>div.control>button.create");
+		buttonTrack.addEventListener("click",event => this.#invokeCreateOrUpdateTrack(this.#album));
 	}
+	
 
 
 	async #invokeCreateOrUpdateAlbum (album) {
@@ -73,7 +92,26 @@ class EditorTabController extends TabController {
 		if (!response.ok) throw new Error("HTTP " + response.status + " " + response.statusText);
 		return window.parseInt(await response.json());
 	}
+
+	
+	async #invokeQueryAllAlbums () {
+		const resource = this.sharedProperties["service-origin"] + "/services/albums";
+		console.log("resource",resource);
+		const headers = { "Accept": "application/json" };
+		const response = await fetch(resource, { method: "GET" , headers: headers, credentials: "include" });
+		if (!response.ok) throw new Error("HTTP " + response.status + " " + response.statusText);
+		return response.json();
+	}
+
+	async #invokeCreateOrUpdateTrack () {
+        console.log("track page should be here",this.#invokeQueryAllAlbums());
+		const trackTemplate = this.editorTrackTemplate.content.firstElementChild.cloneNode(true);
+		this.serverAlbumEditorSectionTable.append(trackTemplate);
+
+	}
 }
+
+
 
 /*
  * Registers an event handler for the browser window's load event.
